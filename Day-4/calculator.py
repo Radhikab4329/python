759e6417c7f6cf72407d1a98e74c1929e9070ca7
Radhika <radhika.b4329@gmail.com>
2024-03-23 22:50:48 +0530

    python

---
+++ Day-4/calculator.py
@@ -0,0 +1,18 @@
+num1 = 20
+num2 = 4
+
+def addition():
+    add = num1 + num2
+    print(add)
+
+def substraction():
+    sub = num1 - num2
+    print(sub)
+
+def multiplication():
+    mul = num1 * num2
+    print(mul)
+
+addition()
+substraction()
+multiplication()
