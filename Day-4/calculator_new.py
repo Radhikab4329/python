f354eb78dec8fda03d4991b115b059054cfb6cdf
Radhika <radhika.b4329@gmail.com>
2024-03-24 23:46:00 +0530

    python

--- Day-4/calculator_new.py
+++ Day-4/calculator_new.py
@@ -1,4 +1,19 @@
-import calculator as basic_calc
+def addition(num1, num2):
+    add = num1 + num2
+    return add
 
-basic_calc
+def substraction(num1, num2):
+    sub = num1 - num2
+    return sub
+
+def multiplication(num1, num2):
+    mul = num1 * num2
+    return mul
+
+print(addition(10, 5))
+print(substraction(10, 5))
+print(multiplication(10, 5))
+
+
+
 
