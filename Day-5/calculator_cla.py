cca01a1e89f20c1da9b2b3828e6c6f1c3b2832a0
Radhika <radhika.b4329@gmail.com>
2024-03-25 00:25:02 +0530

    python

---
+++ Day-5/calculator_cla.py
@@ -0,0 +1,21 @@
+import sys
+
+def addition(num1, num2):
+    add = num1 + num2
+    return add
+
+def substraction(num1, num2):
+    sub = num1 - num2
+    return sub
+
+def multiplication(num1, num2):
+    mul = num1 * num2
+    return mul
+
+num1 = sys.argv[1]
+operator = sys.argv[2]
+num2 = sys.argv[3]
+
+if operation == "add":
+    output = add(num1, num2)
+
