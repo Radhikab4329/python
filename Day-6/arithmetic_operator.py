8f8e82107a8a36f611ced346a53397d6ac639a25
Radhika <radhika.b4329@gmail.com>
2024-03-25 16:55:34 +0530

    python

---
+++ Day-6/arithmetic_operator.py
@@ -0,0 +1,15 @@
+a = 100
+b = 20
+
+sum_result = a + b
+print("sum:", sum_result)
+
+difference_result = a - b
+print("difference:", difference_result)
+
+product_result = a * b
+print("product:", product_result)
+
+quotient_result = a / b
+print("quotient:", quotient_result)
+
