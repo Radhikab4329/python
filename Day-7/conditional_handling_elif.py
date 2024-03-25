892cb7c9ee24fa94fb02202fb129a8f4965ef8b7
Radhika <radhika.b4329@gmail.com>
2024-03-25 22:02:26 +0530

    python

---
+++ Day-7/conditional_handling_elif.py
@@ -0,0 +1,16 @@
+import sys
+
+type = sys.argv[1]
+
+if type == "t2.micro":
+    print("It will charge you 2 dollars a day")
+
+elif type == "t2.medium":
+    print("It will charge you 4 dollars a day")
+
+elif type == "t2.xlarge":
+    print("It will charge you 8 dollars a day")
+
+else:
+    print("provide correct instance type")
+
