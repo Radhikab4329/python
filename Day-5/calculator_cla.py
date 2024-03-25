0a0f77bf225a5d0d9718f84679d5a1f07f43515b
Radhika <radhika.b4329@gmail.com>
2024-03-25 14:31:46 +0530

    python

--- Day-5/calculator_cla.py
+++ Day-5/calculator_cla.py
@@ -13,11 +13,17 @@ def multiplication(num1, num2):
     return mul
 
 num1 = float(sys.argv[1])
-operator = sys.argv[2]
+operation = sys.argv[2]
 num2 = float(sys.argv[3])
 
 if operation == "add":
     output = addition(num1, num2)
     print(output)
 
+if operation == "sub":
+    output = substraction(num1, num2)
+    print(output)
 
+if operation == "mul":
+    output = multiplication(num1, num2)
+    print(output)
